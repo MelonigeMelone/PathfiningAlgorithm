ea7f46a894809418d53c25eda57bc2f989363141
MelonigeMelone <eberleto@web.de>
2022-05-14 12:40:31 +0200

    Updated

--- src/main/java/de/melonigemelone/pathfinder/Pathfinder.java
+++ src/main/java/de/melonigemelone/pathfinder/Pathfinder.java
@@ -1,7 +1,6 @@
 package de.melonigemelone.pathfinder;
 
-
-import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -11,7 +10,7 @@ public class Pathfinder {
     /**
      * Alle möglichen Nachbar Positionen
      */
-    private final Vector2D[] neighbourPositionsWithDiagonal = new Vector2D[] {
+    private static final Vector2D[] neighbourPositionsWithDiagonal = new Vector2D[] {
             new Vector2D(1,0),
             new Vector2D(0,1),
             new Vector2D(1,1),
@@ -23,75 +22,83 @@ public class Pathfinder {
     };
 
     /**
-     * Aktuell offene Nodes
+     * Weite und Höhe des Grids
      */
-    private final CopyOnWriteArrayList<Node> openNodes = new CopyOnWriteArrayList<>();
-
-    /**
-     * Aktuell geschlossene Nodes
-     */
-    private final CopyOnWriteArrayList<Node> closedNodes = new CopyOnWriteArrayList<>();
+    private static int gridWidth, gridHeight;
 
     /**
      * Hindernisse
      */
-    private final List<Vector2D> blockedNodes;
+    private static List<Vector2D> blockedNodes;
+
+    /**
+     * Aktuell offene Nodes
+     */
+    private static final CopyOnWriteArrayList<Node> openNodes = new CopyOnWriteArrayList<>();
+
+    /**
+     * Aktuell geschlossene Nodes
+     */
+    private static final CopyOnWriteArrayList<Node> closedNodes = new CopyOnWriteArrayList<>();
 
     /**
      * Ziel-Node
      */
-    private Node finalNode;
+    private static Node finalNode;
 
     /**
-     * Standard Konstruktor
+     * Initialisiert den Pathfinder
      *
-     * @param blockedNodes Hindernisse
+     * @param gridWidth Weite des Grids
+     * @param gridHeight Höhe des Grids
+     * @param blockedPositions Geblockte Positionen
      */
-    public Pathfinder(List<Vector2D> blockedNodes) {
-        this.blockedNodes = blockedNodes;
+    public static void init(int gridWidth, int gridHeight, List<Vector2D> blockedPositions) {
+        Pathfinder.gridWidth = gridWidth;
+        Pathfinder.gridHeight = gridHeight;
+        Pathfinder.blockedNodes = blockedPositions;
     }
 
     /**
      * Sucht einen Weg
      *
-     * @param start Startpunkt
-     * @param target Zielpunkt
-     * @param grid Grid
-     * @param graphics Zeichenfläche
+     * @param startPos Startpunkt
+     * @param targetPos Zielpunkt
+     * @param size Größe eines Objekts
      */
-    public void findPath(Node start, Node target, Grid grid, Graphics graphics) {
+    public static Vector2D[] findPath(Vector2D startPos, Vector2D targetPos, double size) {
+        Node start = new Node(startPos, 0);
+        Node target = new Node(targetPos, 0);
         openNodes.add(start);
 
         while(true) {
             Node currentNode = getOpenNodeWithLowestFCosts();
             openNodes.remove(currentNode);
             closedNodes.add(currentNode);
-            grid.fillGridPosition(graphics, currentNode.getVector2D(), Color.PINK);
 
             //Pfad gefunden
             if(currentNode.equals(target)) {
                 finalNode = currentNode;
-                drawFinalPath(grid, graphics);
-                return;
+                return getWaypoints();
             }
 
             Vector2D[] neighbours = getNeighboursNodesPositions(currentNode.getVector2D());
             for(Vector2D vector2D : neighbours) {
                 if(!checkIfNodeIsBlocked(vector2D) && !checkIfNodeIsInClosedList(vector2D)) {
-                    if(checkIfNodeIsInOpenList(vector2D)) {
-                        Node node = getNodeFromPosition(vector2D);
-                        int newFCosts = calculateNodeCosts(start, target, Objects.requireNonNull(node));
-                        if(newFCosts < node.getFCost()) {
+                    if (checkSize(vector2D, size)) {
+                        if (checkIfNodeIsInOpenList(vector2D)) {
+                            Node node = getNodeFromPosition(vector2D);
+                            int newFCosts = calculateNodeCosts(target, Objects.requireNonNull(node));
+                            if (newFCosts < node.getFCost()) {
+                                node.setParent(currentNode);
+                                node.setFCost(newFCosts);
+                            }
+                        } else {
+                            Node node = new Node(vector2D, 0);
                             node.setParent(currentNode);
+                            int newFCosts = calculateNodeCosts(target, node);
                             node.setFCost(newFCosts);
                         }
-                    } else {
-                        Node node = new Node(vector2D, 0);
-                        node.setParent(currentNode);
-                        int newFCosts = calculateNodeCosts(start, target, node);
-                        node.setFCost(newFCosts);
-                        openNodes.add(node);
-                        grid.fillGridPosition(graphics, node.getVector2D(), Color.YELLOW);
                     }
                 }
             }
@@ -99,18 +106,52 @@ public class Pathfinder {
     }
 
     /**
-     * Zeichnet den finalen Pfad
+     * Überprüft, ob die Umgebung um die Position herum groß genug für das Objekt ist
      *
-     * @param grid Grid
-     * @param graphics Zeichenfläche
+     * @param pos momentane Position
+     * @param size Größe des Objektes
      */
-    private void drawFinalPath(Grid grid, Graphics graphics) {
+    private static boolean checkSize(Vector2D pos, double size) {
+        int loop = (int) Math.round(size / 2d);
+
+        for(int i = 0; i<loop; i++) {
+            for(int x = -loop; x<=i; x++) {
+                for(int y = -loop; y<=i; y++) {
+                    Vector2D vector2D = new Vector2D(pos.getX()+x, pos.getY()+y);
+                    if(vector2D.getX() < gridWidth && vector2D.getX() > 0 && vector2D.getY() < gridHeight && vector2D.getY() > 0) {
+                        if (checkIfNodeIsBlocked(vector2D)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Gibt alle Wegpunkte zurück
+     *
+     * @return Liste aller Wegpunkte
+     */
+    private static Vector2D[] getWaypoints() {
+        List<Vector2D> list = new ArrayList<>();
+
         Node currentNode = finalNode;
         while(currentNode.getParent() != null) {
-            grid.fillGridPosition(graphics, currentNode.getVector2D(), Color.BLUE);
-            System.out.println(currentNode.getFCost());
+            list.add(currentNode.getVector2D());
             currentNode = currentNode.getParent();
         }
+
+        Vector2D[] waypoints = new Vector2D[list.size()];
+        int counter = list.size()-1;
+        for(Vector2D vector2D : list) {
+            waypoints[counter] = vector2D;
+            counter--;
+        }
+
+        return waypoints;
     }
 
     /**
@@ -118,7 +159,7 @@ public class Pathfinder {
      *
      * @return Node mit den kleinsten Kosten
      */
-    private Node getOpenNodeWithLowestFCosts() {
+    private static Node getOpenNodeWithLowestFCosts() {
         Node tempNode = openNodes.get(0);
 
         for (Node openNode : openNodes) {
@@ -135,7 +176,7 @@ public class Pathfinder {
      * @param currentPosition aktuelle Position
      * @return Feld an Nachbar-Positionen
      */
-    private Vector2D[] getNeighboursNodesPositions(Vector2D currentPosition) {
+    private static Vector2D[] getNeighboursNodesPositions(Vector2D currentPosition) {
         Vector2D[] positions = new Vector2D[8];
         for(int i = 0; i< neighbourPositionsWithDiagonal.length; i++) {
             positions[i] = new Vector2D(currentPosition.getX() + neighbourPositionsWithDiagonal[i].getX(), currentPosition.getY() + neighbourPositionsWithDiagonal[i].getY());
@@ -149,7 +190,7 @@ public class Pathfinder {
      * @param vector2D Position
      * @return geblockt
      */
-    private boolean checkIfNodeIsBlocked(Vector2D vector2D) {
+    private static boolean checkIfNodeIsBlocked(Vector2D vector2D) {
         for(Vector2D vector2D1 : blockedNodes) {
             if(vector2D1.equals(vector2D)) {
                 return true;
@@ -164,7 +205,7 @@ public class Pathfinder {
      * @param vector2D Position
      * @return geschlossen
      */
-    private boolean checkIfNodeIsInClosedList(Vector2D vector2D) {
+    private static boolean checkIfNodeIsInClosedList(Vector2D vector2D) {
         for(Node node : closedNodes) {
             if(node.getVector2D().equals(vector2D)) {
                 return true;
@@ -179,7 +220,7 @@ public class Pathfinder {
      * @param vector2D Position
      * @return offen
      */
-    private boolean checkIfNodeIsInOpenList(Vector2D vector2D) {
+    private static boolean checkIfNodeIsInOpenList(Vector2D vector2D) {
         for(Node node : openNodes) {
             if(node.getVector2D().equals(vector2D)) {
                 return true;
@@ -194,7 +235,7 @@ public class Pathfinder {
      * @param vector2D Position
      * @return Node
      */
-    private Node getNodeFromPosition(Vector2D vector2D) {
+    private static Node getNodeFromPosition(Vector2D vector2D) {
         for(Node node : openNodes) {
             if(node.getVector2D().equals(vector2D)) {
                 return node;
@@ -206,35 +247,17 @@ public class Pathfinder {
     /**
      * Berechnet die Kosten einer Node
      *
-     * @param start Startpunkt
      * @param target Zielpunkt
      * @param node aktuelle Node
      * @return Kosten
      */
-    private int calculateNodeCosts(Node start, Node target, Node node) {
-        Node tempNode = node, tempParent = node.getParent();
+    private static int calculateNodeCosts(Node target, Node node) {
+        Node tempParent = node.getParent();
 
         int parentCosts = tempParent.getStartCost();
-        int nodeCosts = calculateCostsBetweenNodes(tempNode, tempParent);
+        int nodeCosts = calculateCostsBetweenNodes(node, tempParent);
         int gCost = parentCosts + nodeCosts;
         node.setStartCost(gCost);
-
-        /*
-        Berechnet die Startkosten
-
-        Hierfür wird von der aktuellen Node der Parent genommen und die Distanz berechnet, diese dann zu den Gesamtkosten addiert.
-        Danach vom Parent der Parent solange bis man wieder am Start angekommen ist.
-         */
-     /*   while(!end) {
-            if(tempNode.getVector2D().equals(start.getVector2D())) {
-                end = true;
-            } else {
-                int tempCosts = calculateCostsBetweenNodes(tempNode, tempParent);
-                gCost += tempCosts;
-                tempNode = tempParent;
-                tempParent = tempNode.getParent();
-            }
-        }*/
 
         //Berechnung der Kosten zum Zielpunkt
         int hCost = calculateCostsBetweenNodes(node, target);
@@ -249,7 +272,7 @@ public class Pathfinder {
      * @param two zweite Node
      * @return Kosten
      */
-    private int calculateCostsBetweenNodes(Node one, Node two) {
+    private static int calculateCostsBetweenNodes(Node one, Node two) {
 
         //Differenz als Betrag
         Vector2D difference = new Vector2D(Math.abs(two.getVector2D().getX() - one.getVector2D().getX()), Math.abs(two.getVector2D().getY() - one.getVector2D().getY()));
--- src/main/java/de/melonigemelone/pathfinder/Window.java
+++ src/main/java/de/melonigemelone/pathfinder/Window.java
@@ -2,6 +2,7 @@ package de.melonigemelone.pathfinder;
 
 import javax.swing.*;
 import java.awt.*;
+import java.nio.file.Path;
 import java.util.Arrays;
 
 public class Window extends JFrame {
@@ -9,35 +10,25 @@ public class Window extends JFrame {
     private final Grid grid;
 
     private Pathfinder pathfinder;
-    private final Vector2D start = new Vector2D(15,15), finish = new Vector2D(1500,800);
+    private final Vector2D start = new Vector2D(15,15), finish = new Vector2D(50,15);
 
     public Window() {
 
-        this.grid = new Grid(1,1600,900, 500000);
-      /*  this.grid = new Grid(10,80,80, new Vector2D[] {
-                new Vector2D(12,12),
-                new Vector2D(13,12),
-                new Vector2D(14,12),
-                new Vector2D(15,12),
-                new Vector2D(16,11),
-                new Vector2D(16,12),
-                new Vector2D(16,10),
-                new Vector2D(12,9),
-                new Vector2D(13,9),
-                new Vector2D(14,9),
-                new Vector2D(15,9),
+     //   this.grid = new Grid(10,160,90, 200);
+       this.grid = new Grid(10,80,80, new Vector2D[] {
+                new Vector2D(20,13),
+                new Vector2D(20,14),
+                new Vector2D(20,17),
+                new Vector2D(20,18)
 
-        });*/
+        });
        // this.pathFinder = new PathFinder(grid,start, finish, true );
-        pathfinder = new Pathfinder(Arrays.asList(grid.getBlockedFields()));
 
 
         setTitle("Tets");
         setSize(new Dimension(1600,900));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
-
-        pathfinder.findPath(new Node(start, 0), new Node(finish, 0), grid, getGraphics());
 
     }
 
